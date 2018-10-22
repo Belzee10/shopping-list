7cf6159a37ebad5eeb97c95bd7eb8efeab537aa4
Belzee10 <ivan.perez@pri.jovenclub.cu>
2018-10-22 02:08:58 -0500

    redux boilerplate finished

---
+++ client/src/components/ShoppingList.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import { Container, ListGroup, ListGroupItem, Button } from "reactstrap";
+import uuid from "uuid";
+import { connect } from "react-redux";
+import { getItems } from "../actions/itemActions";
+import PropTypes from "prop-types";
+
+class ShoppingList extends Component {
+  componentDidMount() {
+    this.props.getItems();
+  }
+
+  render() {
+    const { items } = this.props.item;
+    return (
+      <Container>
+        <Button
+          color="dark"
+          style={{ marginBottom: "2rem" }}
+          onClick={() => {
+            const name = prompt("Enter Item");
+            if (name) {
+              this.setState({
+                items: [...this.state.items, { id: uuid(), name }]
+              });
+            }
+          }}
+        >
+          Add Item
+        </Button>
+        <ListGroup>
+          {items.map(({ id, name }) => (
+            <ListGroupItem key={id}>
+              <Button
+                className="btn-remove"
+                color="danger"
+                size="sm"
+                onClick={() => {
+                  this.setState(state => ({
+                    items: state.items.filter(item => item.id !== id)
+                  }));
+                }}
+              >
+                &times;{" "}
+              </Button>
+              {name}
+            </ListGroupItem>
+          ))}
+        </ListGroup>
+      </Container>
+    );
+  }
+}
+
+ShoppingList.propTypes = {
+  getItems: PropTypes.func.isRequired,
+  item: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  item: state.item
+});
+
+export default connect(
+  mapStateToProps,
+  { getItems }
+)(ShoppingList);
