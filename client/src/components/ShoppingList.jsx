d69382f143bae879f30ce92a27e7d89727b845c2
Belzee10 <ivan.perez@pri.jovenclub.cu>
2018-10-22 03:37:13 -0500

    app conected to the BACKEND

--- client/src/components/ShoppingList.jsx
+++ client/src/components/ShoppingList.jsx
@@ -18,13 +18,13 @@ class ShoppingList extends Component {
     return (
       <Container>
         <ListGroup>
-          {items.map(({ id, name }) => (
-            <ListGroupItem key={id}>
+          {items.map(({ _id, name }) => (
+            <ListGroupItem key={_id}>
               <Button
                 className="btn-remove"
                 color="danger"
                 size="sm"
-                onClick={this.onDeleteClick.bind(this, id)}
+                onClick={this.onDeleteClick.bind(this, _id)}
               >
                 &times;{" "}
               </Button>
